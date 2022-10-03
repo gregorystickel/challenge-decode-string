b5d2954050d404b4739ac21f838defa5641b2b35
Greg Stickel <gregory.stickel@live.com>
2022-10-03 09:47:59 -0400

    Final commit

--- decodeString.js
+++ decodeString.js
@@ -1,1 +1,15 @@
 // Write your code below this line
+
+function decodeString(sequence) {
+  let arr = [];
+  let result = "";
+  num = parseInt(sequence[0]);
+  for (let i = 1; i < sequence.length; i++) {
+    arr.push(String.fromCharCode(sequence[i].charCodeAt() + num));    
+  }
+  result = arr.toString().replaceAll(`,`, ``);
+
+  return result;
+}
+
+console.log(decodeString("3ce"));
